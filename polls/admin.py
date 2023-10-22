e0777310ba751e11412dfbca14883c552243846e
shivaniindoria <shivani.indoria30@gmail.com>
2023-10-22 23:04:17 +0530

    added unit test

--- polls/admin.py
+++ polls/admin.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
 from .models import Question, Choice
 
+
 class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 0
+
 
 class QuestionAdmin(admin.ModelAdmin):
     list_display = ('id', 'question_text')
     ordering = ('id',)
     inlines = [ChoiceInline]
+
 
 admin.site.register(Question, QuestionAdmin)
