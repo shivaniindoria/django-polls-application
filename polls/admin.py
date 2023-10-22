36b0dce05ddd48fcf620139c6aaa53a2d4844681
shivaniindoria <shivani.indoria30@gmail.com>
2023-10-22 18:28:49 +0530

    improve admin interface

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from .models import Question, Choice
-# Register your models here.
 
-admin.site.register(Question)
-admin.site.register(Choice)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 0
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'question_text')
+    ordering = ('id',)
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
